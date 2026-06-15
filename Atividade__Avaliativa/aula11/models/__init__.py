74f980eaff6ac4b3567c56640fd2ecdaa0307cef
Davi Maliniak <davimaliniak@gmail.com>
2026-06-15 20:24:32 -0300

    Add files via upload

---
+++ Atividade__Avaliativa/aula11/models/__init__.py
@@ -0,0 +1,16 @@
+# Aqui nasce o "db" — é ele que conversa com o arquivo .db do SQLite.
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# O PONTO (.) no import = "pega da MESMA pasta models/"
+# Ex.: from .cliente = arquivo cliente.py que está do seu lado, no mesmo apartamento.
+# Já no controller a gente usa "from models import Cliente" (sem ponto) porque olhamos de FORA.
+from .base import ModeloBase
+from .cliente import Cliente
+from .pedido import ItemPedido, Pedido
+from .colecionador import Colecionador
+from .figurinha import Figurinha
+from .oferta import OfertaTroca, ItemOferta
+
+__all__ = ["db", "ModeloBase", "Cliente", "Pedido", "ItemPedido","Colecionador","Figurinha","OfertaTroca","ItemOferta",]
